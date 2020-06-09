42052de772675b3d256afc26be0fe80b356ea954
Josh Lyford <Josh.Lyford@blackbaud.me>
2020-06-09 18:10:44 -0400

    new clean function

--- git-functions.sh
+++ git-functions.sh
@@ -20,6 +20,32 @@ gitall() {
     git push --set-upstream origin "$branch"
 }
 
+gitclean(){
+    local local_branch=("${(@f)$(git branch | sed 's|* |  |' | sort)}")
+    local remote_branch=("${(@f)$(git branch -r | sed 's|origin/||' | sort)}")
+    local diff_list=()
+    for i in "${local_branch[@]}"; do
+        skip= 
+        for j in "${remote_branch[@]}"; do
+            [[ $i == $j ]] && { skip=1; break; }
+        done
+        [[ -n $skip ]] || diff_list+=("$i")
+    done
+    for b in "${diff_list[@]}"; do
+        git branch -D $(echo $b | xargs)
+    done
+}
+
+gitl(){
+    local lfile=$(git branch | sed 's|* |  |' | sort)
+    echo $lfile
+}
+
+gitr(){
+    local rfile=$(git branch -r | sed 's|origin/||' | sort)
+    echo $rfile
+}
+
 gitb() {
     local prefix=""
     if [ $ZSHE_USER ]
@@ -35,6 +61,3 @@ gitb() {
         git checkout -b "${prefix}feature-$frd"
     fi
 }
-
-
-alias gitclean="git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D" 
